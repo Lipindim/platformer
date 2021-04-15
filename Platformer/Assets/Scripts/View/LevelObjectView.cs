3ac27a532be9f572b559b91eea2bade177b94698
lipindim <lipindim@gmail.com>
2021-04-15 21:00:18 +1000

    Домашняя работа 1

---
+++ Platformer/Assets/Scripts/View/LevelObjectView.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+
+public class LevelObjectView : MonoBehaviour
+{
+    public Transform Transform;
+    public SpriteRenderer SpriteRenderer;
+    public Rigidbody2D Rigidbody2D;
+    public Collider2D Collider2D;
+}
