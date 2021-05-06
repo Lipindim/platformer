40ad406a7194059a4e132fbe5a0baebdc8068084
lipindim <lipindim@gmail.com>
2021-05-06 10:07:10 +1000

    Домашняя работа 5

---
+++ Platformer/Assets/Scripts/View/CharacterView.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+
+public class CharacterView : MonoBehaviour
+{
+    #region Fields
+
+    public Transform Transform;
+    public SpriteRenderer SpriteRenderer;
+    public Rigidbody2D Rigidbody2D;
+    public Collider2D Collider2D;
+    public TrailRenderer Trail;
+    
+    public Action<CharacterView> OnLevelObjectContact { get; set; }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        var levelObject = other.gameObject.GetComponent<CharacterView>();
+        OnLevelObjectContact?.Invoke(levelObject);
+    }
+
+    #endregion
+}
